f0d71efd2fd1254f0cebab139086926ce5a97daf
Baptiste Lhopitault <baptiste@repliq.fr>
2023-07-13 17:34:58 +0200

    Connect template with template

---
+++ templates/documentation.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?= css('/media/plugins/baptiste/kirby-documentation-plugin/styles/main.css') ?>
+</head>
+<body>
+    <h1>Hello</h1>
+</body>
+</html>
